b34738b4f64da7cf9c286fbfef5cb482a8ba865f
William Falconer <wfalconer7272@arecibointer.edu>
2022-09-17 15:09:04 -0500

    Update index.js
    Uncommented code that enables "Try it" button

--- js/index.js
+++ js/index.js
@@ -63,32 +63,32 @@ function addFeatureToMap(data){
 }
 
 
-// function getLocation(x) {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition,x);
-//   } else {
-//     x.innerHTML = "Geolocation is not supported by this browser.";
-//   }
-// }
+function getLocation(x) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition,x);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
 
-// function showPosition(position,x) {
-//   document.getElementById("location").innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-// }
+function showPosition(position,x) {
+  document.getElementById("location").innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
 
 
-// function showError(error) {
-//   switch(error.code) {
-//     case error.PERMISSION_DENIED:
-//       x.innerHTML = "User denied the request for Geolocation."
-//       break;
-//     case error.POSITION_UNAVAILABLE:
-//       x.innerHTML = "Location information is unavailable."
-//       break;
-//     case error.TIMEOUT:
-//       x.innerHTML = "The request to get user location timed out."
-//       break;
-//     case error.UNKNOWN_ERROR:
-//       x.innerHTML = "An unknown error occurred."
-//       break;
-//   }
-// }
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+}
